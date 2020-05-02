8dfd9802fa2d72768eabba4465a5a8dcb9be8a31
akmalubayy <mhmdakmal46@gmail.com>
2020-05-02 10:58:04 +0700

    Initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import moduleName from 'elements/Button';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
